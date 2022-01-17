5b94584fb2df7c4400b443ff59c3b5a2f4ba7bab
Erwanlbv <er.leblevec@gmail.com>
2022-01-17 23:28:59 +0100

    Ré-organisation du programme,

--- Exercice_2/Questions_p2.py
+++ Exercice_2/Questions_p2.py
@@ -1,5 +1,53 @@
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import numpy as np
+import os
+
+from Donnees_Moodle.filtrage_particulaire import *
 
 from main_p2 import *
 from variables_p2 import *
+from programmes_annexes import *
 
+
+def question_filt_ll_unique():
+    prem_image, names, nb_images, folder_name = lecture_image()
+    res_obs = selectionner_zone() #Format : [x, y, long, larg]
+
+    long = res_obs[2] # On suppose la longueur
+    larg = res_obs[3] # Idem pour la largeur
+    print('Longueur, Largeur : ' + str(long) + ', ' + str(larg))
+    hist_obs = calcul_histogramme(prem_image, res_obs, Nb)[-1] #On enregistre lamb'histrogramme de couleur du visage
+
+    w_init = 1/N * np.ones(N)
+    filtrage_particulaire_unit(prem_image, res_obs, hist_obs, long, larg, w_init)
+
+
+def question_filt_ll_fixes():
+    prem_image, names, nb_images, folder_name = lecture_image()
+    res_obs = selectionner_zone() #Format : [x, y, long, larg]
+
+    long = res_obs[2] # On suppose la longueur
+    larg = res_obs[3] # Idem pour la largeur
+    img_obs, colormap, hist_obs = calcul_histogramme(prem_image, res_obs, Nb) #On enregistre l'histrogramme de couleur du visage
+
+    if not(os.path.isfile('filtrage_sequence_3.txt')):
+        W = np.zeros((T, N))
+        X = np.zeros((T, N, 2))
+        ests = np.zeros((N, 2))
+        w_init = 1/N * np.ones(N)
+        w_seuil = 0.6
+
+        X[0], W[0], ests[0] = filtrage_particulaire_unit(prem_image, res_obs, hist_obs, colormap, w_init)
+
+        for i in range(1, nb_images):
+            X[i], W[i], ests[i] = filtrage_part_global(i, long, larg, X[i-1], W[i-1], hist_obs, colormap, w_seuil)
+
+        np.savetxt('filtrage_sequence_3.txt', ests) #Sauvegarde en conservant le format d'un array numpy
+
+    print('--- Fin du filtrage --- ')
+
+#question_filt_ll_unique()
+#question_filt_ll_fixes()
+images_to_videos()
+#make_histo()
---
+++ Exercice_2/programmes_annexes.py
@@ -0,0 +1,75 @@
+import time
+
+import cv2 as cv
+import matplotlib.pyplot
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+from Donnees_Moodle.filtrage_particulaire import *
+
+
+def est_to_images(image, est, long, larg, path): # Affiche et sauvegarde la prédiction sur l'image
+    fig, ax = plt.subplots()
+    ax.imshow(image)
+    rect = patches.Rectangle(est, long, larg, linewidth=1, edgecolor='r', facecolor='none')
+    ax.add_patch(rect)
+
+    plt.pause(0.01)
+
+    fig.savefig(path)
+    plt.close(fig)
+
+
+def images_to_videos():
+    im = cv.imread('../Plots/Exercice_2/Suivi_ll_fixes/image_1.png')
+    height, width = im.shape[0], im.shape[1]
+    print(height, width)
+    fourcc = cv.VideoWriter_fourcc(*'mp4v')
+    video = cv.VideoWriter('video.mp4', fourcc, 1.5, (width, height))
+
+    for i in range(48):
+        img = cv.imread('../Plots/Exercice_2/Suivi_ll_fixes/image_' + str(i) + '.png')
+        video.write(img)
+
+    cv.destroyAllWindows()
+    video.release()
+
+    cap = cv.VideoCapture('video.mp4')
+
+    if not(cap.isOpened()):
+        print("Error opening video  file")
+
+    while True:
+        ret, frame = cap.read()
+        time.sleep(0.5)
+        cv.imshow('output', frame)
+
+        if (cv.waitKey(1) & 0xFF == ord('q')):
+            break
+
+    cap.release()
+    cv.destroyAllWindows()
+
+
+def test_on_display():
+    im = plt.imread('Donnees_Moodle/sequences/sequence1/sequence10004.bmp')
+
+    fig, ax = plt.subplots()
+    ax.imshow(im)
+    rect = patches.Rectangle((2, 2), 10, 10, linewidth=1, edgecolor='r', facecolor='none')
+    ax.add_patch(rect)
+
+    fig.show()
+    fig.savefig('Test_Display.png')
+
+
+def make_histo():
+    im = Image.open('../Plots/Exercice_2/Suivi_ll_fixes/image_1.png')
+
+    fig1 = plt.figure()
+    plt.imshow(im)
+
+    fig2 = plt.figure()
+    plt.plot(im.histogram())
+    plt.show()
